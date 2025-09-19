2f646fd12ebad77ff2ba8af909e3b69138cd6bab
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-09-18 23:41:37 -0400

    style: configuration of tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#8B5CF6", // violet-500
+          dark: "#7C3AED",
+          light: "#A78BFA"
+        }
+      },
+      borderRadius: {
+        xl: "1rem",
+      }
+    },
+  },
+  plugins: [],
+}
+
